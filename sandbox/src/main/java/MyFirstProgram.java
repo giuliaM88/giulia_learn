81919d5589c5977ad17d3e29651126db583b9e78
giuliaM88 <giuliaM88@users.noreply.github.com>
2020-10-26 00:32:30 +0300

    создан проект "песочница"

---
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -0,0 +1,11 @@
+//package ru.stqa.learn.sandbox;
+
+public class MyFirstProgram {
+	public static void main(String[] args){
+		System.out.println("Hello!");
+		//Square s = new Square(5);
+	   // Rectangle r = new Rectangle(4, 6);//это конструктор
+	  //  System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+		//System.out.println("Площадь прямоуг-ка со сторонами " + r.a + " и " + r.b + " = " + r.area());
+	}
+}
